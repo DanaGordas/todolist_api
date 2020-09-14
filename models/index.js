51a615c42b09fb83aad0bc1df27115d8326abb55
Daniela Gordas <daniela.gordas@gmail.com>
2020-09-14 19:18:48 +0200

    fixing MongoDB deployment using Atlas

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 mongoose.set('debug', true);
 
 // connect to DB
-mongoose.connect('mongodb://localhost/todo-api' || process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo-api', { useNewUrlParser: true });
 
 mongoose.Promise = Promise;
 
